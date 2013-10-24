22d9fbaf053f9517e8296efd506da02b3057c847
Wen Li <liwen6023@gmail.com>
2013-10-23 20:57:01 -0400

    Initial commit

---
+++ de.vogella.git.first/src/de/vogella/git/first/gitTest.java
@@ -0,0 +1,10 @@
+package de.vogella.git.first;
+
+public class gitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git is fun!");
+	}
+
+}
